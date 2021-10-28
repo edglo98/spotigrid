9e63fe69ffde87aa8b31094277bd4f8abe739c74
Edgar Lopez <edgarlopezmgs@live.com>
2021-10-28 15:59:28 -0500

    feat: loging page built

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import styles from './styles.module.css'
 import { images } from '../../assets/images'
+import { scrapingAlbums } from '../../assets/resources/scrapingAlbums'
 import SpotifyLoginButton from '../../components/SpotifyLoginButton/SpotifyLoginButton'
+
+const renderAlbums = scrapingAlbums.map(albumSrc => {
+  const urlSplited = albumSrc.split('/')
+  const idAlbum = urlSplited[urlSplited.length - 1]
+
+  return (
+    <span
+      key={idAlbum}
+      style={{ backgroundImage: `url(${albumSrc})` }}
+      className={styles.albumImage}
+    />
+  )
+})
 
 const LoginPage = () => {
   return (
     <div style={{ height: '100%', display: 'grid', placeItems: 'center' }}>
-      <section style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 15 }}>
+      <section className={styles.bgLogin}>
+        {renderAlbums}
+      </section>
+      <section className={styles.loginForm}>
         <img src={images.spotifyLogo} style={{ height: 90, width: 90 }} />
         <h1>Spotigrid</h1>
         <SpotifyLoginButton />
