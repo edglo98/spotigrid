c31a014cd60942ba073fbd2d71a091f8d7590b91
Edgar Lopez <edgarlopezmgs@live.com>
2021-11-03 19:33:11 -0600

    feat: main grid layout

--- src/components/TrackCard/TrackCard.jsx
+++ src/components/TrackCard/TrackCard.jsx
@@ -1,15 +1,44 @@
-import { useRef, useState } from 'react'
+import { forwardRef, useImperativeHandle, useRef, useState } from 'react'
 import { MdPlayCircleOutline, MdPauseCircleOutline } from 'react-icons/md'
 import LinkButton from '../LinkButton/LinkButton'
 import styles from './styles.module.css'
 
-const TrackCard = ({ track, onPressMedia }) => {
+const TrackCard = forwardRef(({ track, onPressMedia, size = null }, forwardRefElement) => {
   const [playing, setPlaying] = useState(false)
   const trackRef = useRef(null)
-  const cantPlay = track.preview_url === null
+  const elementRef = useRef(null)
+  const cantPlay = track.preview === null
+  const [transform, setTranform] = useState({
+    delta: 1,
+    zindex: 100
+  })
+
+  const distanceBetweenPoints = (point1, point2) => {
+    const x = Math.pow(point1.x - point2.x, 2)
+    const y = Math.pow(point1.y - point2.y, 2)
+    return Math.sqrt(x + y)
+  }
+
+  const changeDistance = (origin) => {
+    const rect = elementRef.current.getBoundingClientRect()
+    const coords = {
+      x: rect.left + rect.width / 2,
+      y: rect.top + rect.height / 2
+    }
+    const calculatedDistance = distanceBetweenPoints(origin, coords)
+    const delta = 2 - calculatedDistance / (rect.width * 1.35)
+    const limitedDelta = Math.max(delta, 1)
+    const zIndex = Math.ceil(limitedDelta * 100)
+    return setTranform({ delta: limitedDelta, zindex: zIndex })
+  }
+
+  useImperativeHandle(forwardRefElement, () => ({
+    changeDistance,
+    domElement: elementRef.current
+  }))
 
   const handlePlay = () => {
-    if (track.preview_url) {
+    if (track.preview) {
       if (onPressMedia) {
         onPressMedia(track.id, trackRef.current, setPlaying)
       }
@@ -19,16 +48,25 @@ const TrackCard = ({ track, onPressMedia }) => {
   }
 
   return (
-    <div className={styles.trackContainer}>
-      <span className={styles.coverImage} style={{ backgroundImage: `url(${track.album.images[1].url})` }} />
-      <audio ref={trackRef} src={track.preview_url} />
+    <div
+      style={{
+        width: size ?? '100%',
+        transition: 'transform 0.5s ease',
+        transform: `scale3d(${transform.delta}, ${transform.delta}, ${transform.delta})`,
+        zIndex: transform.zindex
+      }}
+      ref={elementRef}
+      className={styles.trackContainer}
+    >
+      <span className={styles.coverImage} style={{ backgroundImage: `url(${track.image})` }} />
+      <audio ref={trackRef} src={track.preview} />
       <div className={styles.infoContent}>
         <button disabled={cantPlay} className={`${styles.mediaButton} ${cantPlay ? styles.mediaButtonNoPrev : null}`} onClick={handlePlay}>
           {playing ? <MdPauseCircleOutline /> : <MdPlayCircleOutline />}
         </button>
         <div className={styles.info}>
           <p className={styles.trackName}>{track.name}</p>
-          <p className={styles.trackAlbum}>{track.artists[0].name} &#8226; {track.album.name}</p>
+          <p className={styles.trackAlbum}>{track.artists[0]} &#8226; {track.album}</p>
         </div>
         <LinkButton
           iconSize={18}
@@ -39,6 +77,6 @@ const TrackCard = ({ track, onPressMedia }) => {
       </div>
     </div>
   )
-}
+})
 
 export default TrackCard
--- src/pages/app/SpotigridPage/SpotigridPage.jsx
+++ src/pages/app/SpotigridPage/SpotigridPage.jsx
@@ -1,10 +1,101 @@
-import React from 'react'
+import { decode } from 'js-base64'
+import { useLayoutEffect, useRef, useState } from 'react'
+import { useParams } from 'react-router'
+import BetterScroll from 'better-scroll'
+import styles from './styles.module.css'
+import TrackCard from '../../../components/TrackCard/TrackCard'
 
 const SpotigridPage = () => {
+  const { tracks } = useParams()
+  const decoded = decode(tracks)
+  const trackList = JSON.parse(decoded)
+  const [audioPlay, setAudioPlay] = useState({})
+  const mainRef = useRef(null)
+  const songsRef = useRef([])
+  let bscroll = null
+  const maxElementsPerRow = 6
+
+  const getCenter = (node) => {
+    const x = node.offsetLeft + node.offsetWidth / 2
+    const y = node.offsetTop + node.offsetHeight / 2
+    return { x, y }
+  }
+
+  const isMobile = () => {
+    const sizes = {
+      mobile: 500,
+      tablet: 1024
+    }
+    const query = `(max-width: ${sizes.mobile}px)`
+
+    const media = window.matchMedia(query)
+    return media.matches
+  }
+  const currentSize = isMobile() ? 160 : 220
+
+  const handlePlayAudio = (trackId, htmlTrack, setPlaying) => {
+    if (audioPlay.trackId === trackId) return
+    if (audioPlay.trackId && audioPlay.trackId !== trackId) {
+      audioPlay.htmlTrack.pause()
+      audioPlay.setPlaying(false)
+    }
+    setAudioPlay({ trackId, htmlTrack, setPlaying })
+  }
+
+  useLayoutEffect(() => {
+    bscroll = new BetterScroll(mainRef.current, {
+      freeScroll: true,
+      scrollX: true,
+      scrollY: true,
+      probeType: 3
+    })
+
+    const $songs = songsRef.current
+
+    console.log($songs[0].domElement)
+
+    bscroll.scroller.scrollToElement(
+      $songs[Math.floor($songs.length / 2) - 2].domElement,
+      1000,
+      currentSize / 2,
+      currentSize / 2
+    )
+
+    const origin = getCenter(document.body)
+
+    bscroll.on('scroll', (points) => {
+      $songs.forEach(song => {
+        song.changeDistance(origin)
+      })
+    })
+  }, [trackList])
+
   return (
-    <div>
-      spotigrid page
-    </div>
+    <main className={styles.mainTracksContainer} ref={mainRef}>
+      <aside
+        className={styles.listOfTracks}
+        style={{
+          '--width': `${currentSize * maxElementsPerRow}px`,
+          '--height': `${Math.ceil(trackList.length / maxElementsPerRow) * currentSize}px`,
+          '--size': `${currentSize}px`,
+          '--maxElementsPerRow': maxElementsPerRow
+        }}
+      >
+        {
+          trackList.map((track, i) => {
+            return (
+              <TrackCard
+                ref={element => { songsRef.current[i] = element }}
+                size={currentSize}
+                key={track.id}
+                track={track}
+                onPressMedia={handlePlayAudio}
+              />
+            )
+          })
+        }
+      </aside>
+    </main>
   )
 }
 
